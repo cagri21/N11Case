be4c10c900191d719dbf30211770cf8d8236844b
Çağrı Yörükoğlu <cagriyorukoglu21@gmail.com>
2024-11-15 11:57:08 +0100

    Initial Commit

---
+++ N11CaseTests/N11CaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  N11CaseTests.swift
+//  N11CaseTests
+//
+//  Created by Çağrı Yörükoğlu on 15.11.2024.
+//
+
+import Testing
+@testable import N11Case
+
+struct N11CaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ N11CaseUITests/N11CaseUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  N11CaseUITestsLaunchTests.swift
+//  N11CaseUITests
+//
+//  Created by Çağrı Yörükoğlu on 15.11.2024.
+//
+
+import XCTest
+
+final class N11CaseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
