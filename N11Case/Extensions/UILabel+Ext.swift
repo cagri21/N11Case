118091b6c1203e771213b8b1018e6e38eda2151d
Çağrı Yörükoğlu <cagriyorukoglu21@gmail.com>
2024-11-19 19:41:05 +0100

    Currency converter added, cell UI design enhanced.

--- N11Case/Extensions/UILabel+Ext.swift
+++ N11Case/Extensions/UILabel+Ext.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-extension String {
-    func strikeThrough() -> NSAttributedString {
-        let attributeString =  NSMutableAttributedString(string: self)
-        attributeString.addAttribute(
-            NSAttributedString.Key.strikethroughStyle,
-               value: NSUnderlineStyle.single.rawValue,
-                   range:NSMakeRange(0,attributeString.length))
-        return attributeString
+extension UILabel {
+
+func strikeThrough(_ isStrikeThrough:Bool) {
+    if isStrikeThrough {
+        if let lblText = self.text {
+            let attributeString =  NSMutableAttributedString(string: lblText)
+            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0,attributeString.length))
+            self.attributedText = attributeString
+        }
+    } else {
+        if let attributedStringText = self.attributedText {
+            let txt = attributedStringText.string
+            self.attributedText = nil
+            self.text = txt
+            return
+        }
+    }
     }
 }
--- N11Case/Screens/Products/View/Cells/NormalProductCell.swift
+++ N11Case/Screens/Products/View/Cells/NormalProductCell.swift
@@ -16,6 +16,7 @@ protocol ConfigurableCell {
 
 class NormalProductCell: UICollectionViewCell, ConfigurableCell {
 
+
     @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
@@ -28,29 +29,62 @@ class NormalProductCell: UICollectionViewCell, ConfigurableCell {
 
     override func prepareForReuse() {
         super.prepareForReuse()
-        // Reset label text
+        // Reset visibility for stack views and labels
+        titleLabel.isHidden = false
+        priceLabel.isHidden = false
+        discountPriceLabel.isHidden = false // Discount label is hidden by default
+        productImageView.isHidden = false
+
+        // Reset label texts
         titleLabel.text = nil
+        priceLabel.text = nil
+        discountPriceLabel.text = nil
+
         // Reset image view
         productImageView.image = nil
-        // Reset additional cell properties if needed
-        contentView.backgroundColor = .white
+        productImageView.sd_cancelCurrentImageLoad() // Cancel any ongoing image downloads
+
+        // Reset additional properties if any
+        priceLabel.attributedText = nil
+        priceLabel.textColor = UIColor.productDiscountPrice // Reset to default text color
     }
 
     func configure(with product: ProductDisplayable) {
         titleLabel.text = product.title
-        priceLabel.attributedText = product.price.toString().strikeThrough()
-        discountPriceLabel.text = product.instantDiscountPrice.toString()
+        priceLabel.text = CurrencyFormatter.turkey.format(price: product.price)
+
+        if let discountedPrice = product.instantDiscountPrice {
+            discountPriceLabel.isHidden = false
+            priceLabel.strikeThrough(true)
+            discountPriceLabel.text = CurrencyFormatter.turkey.format(price: discountedPrice)
+        } else {
+            priceLabel.strikeThrough(false)
+            priceLabel.textColor = UIColor.productNormalPrice
+            discountPriceLabel.isHidden = true
+        }
+
+        if let rate = product.rate {
+
+        } else {
+
+        }
 
         if product.image.isEmpty {
 
         } else {
-            product.getImage { [weak self] Image in
-                guard let self = self else {
-                    return
-                }
-                self.productImageView.image = Image
-            }
+            productImageView.downloadImage(url: product.image)
         }
     }
+    
+    
 
+    
+}
+extension UIImageView{
+    func downloadImage(url:String){
+      //remove space if a url contains.
+        let stringWithoutWhitespace = url.replacingOccurrences(of: " ", with: "%20", options: .regularExpression)
+        self.sd_imageIndicator = SDWebImageActivityIndicator.gray
+        self.sd_setImage(with: URL(string: stringWithoutWhitespace), placeholderImage: UIImage())
+    }
 }
